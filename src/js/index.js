3a3737481d6b56403aecdc1cc0e75b4b7f8faa1d
Gustavo de Souza <gustavoeliasdesouza2310@gmail.com>
2023-10-11 08:59:12 -0300

    projeto faltando parte do js

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const personagens = document.querySelectorAll(".personagem");
+
+personagens.forEach(personagem => {
+    personagem.addEventListener("mouseenter", () => {
+         const personagemSelecionado = document.querySelector(".selecionado") 
+        personagemSelecionado.classList.remove("selecionado");
+         personagem.classList.add("selecionado");
+        const imagemPersonagemGrande = document.querySelector(".personagem-grande");
+        
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `./src/imagens/imagens/card-${idPersonagem}.png`;
+
+        const nomePersonagem = document.getElementById("nome-personagem");
+        nomePersonagem.innerText = personagem.getAttribute("data-name");
+    })
+})
